e0ed043e885463b6dcbb762439ddc34928be741a
Guy Barrette <guy@guybarrette.com>
2022-08-17 20:37:53 -0400

    Update to 1.8.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://github.com/dapr/cli/releases/download/v1.8.0/dapr_windows_amd64.zip'
-$checksum64    = '9c6c9737f60d3e607f26e93008cf217ae285b36dc1bb3d3b9ef1258c3f7679cd'
+$url           = 'https://github.com/dapr/cli/releases/download/v1.8.1/dapr_windows_amd64.zip'
+$checksum64    = '1de00aae222adcbeee878b37ed2451de5f6470b4e94fdf04bcbb94a554c17bec'
 $checksumType64= 'sha256'
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
