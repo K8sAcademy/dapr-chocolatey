df5ed157f1e4d7e49c93f80191adcdf2ab31b5d8
Guy Barrette <guy@guybarrette.com>
2022-11-19 06:35:41 -0500

    Update to 1.9.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://github.com/dapr/cli/releases/download/v1.8.1/dapr_windows_amd64.zip'
-$checksum64    = '1de00aae222adcbeee878b37ed2451de5f6470b4e94fdf04bcbb94a554c17bec'
+$url           = 'https://github.com/dapr/cli/releases/download/v1.9.1/dapr_windows_amd64.zip'
+$checksum64    = '3fa211179ae0924fefdf969abb83731abdbd463f4c5ef6abf9fb81802efdcefe'
 $checksumType64= 'sha256'
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
