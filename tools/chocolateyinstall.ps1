956e64d139d49daed74b3bbcd25503375b7f498e
Guy Barrette <guy@guybarrette.com>
2022-11-26 17:21:29 -0500

    Added packageSourceUrl and bugTrackerUrl

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://github.com/dapr/cli/releases/download/v1.9.1/dapr_windows_amd64.zip'
+$url64         = 'https://github.com/dapr/cli/releases/download/v1.9.1/dapr_windows_amd64.zip'
 $checksum64    = '3fa211179ae0924fefdf969abb83731abdbd463f4c5ef6abf9fb81802efdcefe'
 $checksumType64= 'sha256'
 
-Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
+Install-ChocolateyZipPackage $env:ChocolateyPackageName $url64 $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
