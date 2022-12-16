1043f44f7c7ca44980f2224df15565de947513c0
Guy Barrette <guy@guybarrette.com>
2022-12-16 11:34:07 -0500

    Revert "Update to 1.9.5"
    This reverts commit c507a18ca39c68d7916e9139cac109f25f77d797.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://github.com/dapr/cli/releases/download/v1.9.5/dapr_windows_amd64.zip'
-$checksum64    = '2b4c3f544741bdf7474a43fee0131909e42705f8df5654e20270def23a9fb3f5'
+$url64         = 'https://github.com/dapr/cli/releases/download/v1.9.1/dapr_windows_amd64.zip'
+$checksum64    = '3fa211179ae0924fefdf969abb83731abdbd463f4c5ef6abf9fb81802efdcefe'
 $checksumType64= 'sha256'
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName $url64 $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
